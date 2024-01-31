fa4cbfb01d129ea2e15444c4c7cbb6a0e166203f
koenweverink <koenweverink@gmail.com>
2024-01-31 14:21:41 +0100

    added powerlaw test, plots and sims

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import argparse
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.colors as mcolors
+import numpy as np
+from scipy.stats import powerlaw
 
 from CellularAutomaton import CellularAutomaton
 
 # Initialize the argument parser
 parser = argparse.ArgumentParser(description='2D Cellular Automaton Star Formation Simulation', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--N', type=int, default=100, help='Grid size')
+parser.add_argument('--N', type=int, default=150, help='Grid size')
 parser.add_argument('--prob_gas', type=float, default=0.15, help='Probability of gas')
 parser.add_argument('--proto_size', type=int, default=35, help='Proto size')
 parser.add_argument('--star_size', type=int, default=200, help='Star size')
@@ -26,6 +28,10 @@ def simulate(N, prob_gas, proto_size, star_size):
     # Initialize the cellular automaton
     automaton = CellularAutomaton(N, p, proto_size, star_size)
 
+    global counts
+    time_step_counter = 0
+    counts = {1: [], 2: [], 3: []}  # Dictionary of lists for each state
+
     # Define colors for each state
     colors = {0: 'white',  # Color for state 0
             1: 'green',   # Color for state 1
@@ -42,13 +48,64 @@ def simulate(N, prob_gas, proto_size, star_size):
 
     # Update function for the animation
     def update(frame):
+        nonlocal time_step_counter
+        if time_step_counter >= 400:
+            plt.close()  # Closes the plot and ends the animation
+            return
+        
+        counts[1].append(sum(agent.state == 1 for row in automaton.grid for agent in row))
+        counts[2].append(sum(agent.state == 2 for row in automaton.grid for agent in row))
+        counts[3].append(sum(agent.state == 3 for row in automaton.grid for agent in row))
+
         mat.set_data(automaton.update(frame))
+
+        time_step_counter += 1
         return [mat]
 
     ani = animation.FuncAnimation(fig, update, interval=1/120, save_count=50)
     plt.show()
 
 
+def check_powerlaw(prob_gas):
+    data = counts[1]
+
+    # Prepare data for analysis
+    hist, bin_edges = np.histogram(data, bins='auto', density=True)
+    bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
+
+    # Fit the data to a power-law distribution
+    a, loc, scale = powerlaw.fit(data)
+
+
+    # Plot the data on a log-log scale
+    plt.loglog(bin_centers, hist, 'o', label='bin_centers')
+    plt.loglog(bin_centers, powerlaw.pdf(bin_centers, a, loc, scale), '-', label='powerlaw fit')
+    plt.xlabel('Count')
+    plt.ylabel('Probability Density')
+    plt.title(f'Log-Log Plot with Power Law Fit (Gas Density {prob_gas})')
+    plt.legend()
+    plt.show()
+
+
 if __name__ == "__main__":
-    # Call the simulate function with arguments from the command line
-    simulate(args.N, args.prob_gas, args.proto_size, args.star_size)
+    # probs_gas = np.arange(0.01, 0.21, 0.05)
+    probs_gas = [0.15]
+
+    for prob_gas in probs_gas:
+        # Call the simulate function with arguments from the command line
+        simulate(args.N, args.prob_gas, args.proto_size, args.star_size)
+        check_powerlaw(prob_gas)
+
+        labels = {1: "gas", 2: "Protostar", 3: "Star"}
+
+        # Plotting the time series data     
+        time_steps = range(len(counts[1]))
+        plt.figure()
+        for state in counts:
+            plt.plot(time_steps, counts[state], label=f'State {labels[state]}')
+        plt.xlabel('Time Step')
+        plt.ylabel('Count')
+        plt.title('State Counts Over Time')
+        plt.legend()
+        plt.xlim(left=150)
+        plt.show()
